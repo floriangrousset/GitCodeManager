fd8adf25d8b03b9fd017bae7593757e6025d9fb5
Florian Grousset <florian@grousset.com>
2023-02-24 22:04:12 -0600

    Now using interactive prompt + doc update

--- README.md
+++ README.md
@@ -13,9 +13,9 @@
 /_/  /_/ \__,_//_/ /_/ \__,_/ \__, / \___//_/     
                              /____/
 </pre>
-Tired of having your GitHub repositories scattered all over your local file system and not necessarily up to date? 
+Tired of having your GitHub repositories scattered all over your local file system and not necessarily up to date?
 
-Git Code Manager is a command-line tool for managing cloned GitHub repositories in a clean fashion on your local file system under the `~/Code/` folder. It helps with keeping your locally cloned repositories always organized and up to date. Current features include cloning repositories from GitHub, displaying the directory tree of cloned repositories along with their latest local and remote commit checksum.
+Git Code Manager is a command-line tool for managing cloned GitHub repositories in a clean fashion on your local file system under the ~/Code/ folder. It helps with keeping your locally cloned repositories always organized and up to date. Current features include cloning repositories from GitHub, displaying the directory tree of cloned repositories along with their latest local and remote commit checksum.
 
 All reposotories are stored in the following directory structure:
 
@@ -30,44 +30,57 @@ More features are coming soon like auto comparison of local and remote repositor
 
 ## Usage
 
-### Cloning a repository with "clone"
-
-The clone command allows users to clone a GitHub repository and store it in the proper directory structure in the local file system. The command takes two parameters: username and repository.
-
+To run the tool, simply run the following command in your terminal:
 ```bash
-$ ./gcm.py clone username repository
+$ ./gcm.py
 ```
 
-Example:
+This will start an interactive menu with two options:
+
+ - Clone a GitHub repository: allows users to clone a GitHub repository and store it in the proper directory structure in the local file system.
+ - List cloned repositories: allows users to display the directory tree of cloned repositories. The command also displays the latest commit checksum of each local and remote repository.
+
+Here is an example of the output:
 
 ```bash
-$ ./gcm.py clone floriangrousset GitCodeManager
-```
-
-### Listing cloned repositories with "list"
-
-The list command allows users to display the directory tree of cloned repositories. The command doesn't take any parameters. It also displays the latest commit checksum of each local and remote repository.
-
-```bash
-$ ./gcm.py list
-```
-
-Output:
-
-```bash
-~/Code
+joshua@WOPR:~/gcm.py 
+==================================================================================================================
+   ______ _  __     ______            __         __  ___                                       
+  / ____/(_)/ /_   / ____/____   ____/ /___     /  |/  /____ _ ____   ____ _ ____ _ ___   _____
+ / / __ / // __/  / /    / __ \ / __  // _ \   / /|_/ // __ `// __ \ / __ `// __ `// _ \ / ___/
+/ /_/ // // /_   / /___ / /_/ // /_/ //  __/  / /  / // /_/ // / / // /_/ // /_/ //  __// /    
+\____//_/ \__/   \____/ \____/ \__,_/ \___/  /_/  /_/ \__,_//_/ /_/ \__,_/ \__, / \___//_/     
+                                                                          /____/               
+==================================================================================================================
+/home/joshua/Code
     |
-    |--floriangrousset
-        |--TestJetBrainFleet | local: None | remote: None
-        +--GitCodeManager | local: 50cc...39da | remote: None
+    |--norad-defunc
+        |--WOPR | local: ae7705f6e3acb04ba1a1db876c07106dc3556fd8 | remote: None
+        +--GitCodeManager | local: 790e229642ed86c3b7e5a8124cf23b0f22c13476 | remote: 790e229642ed86c3b7e5a8124cf23b0f22c13476
     |--JPaulMora
-        +--Pyrit | local: f0f1...782c | remote: f0f1...782c
+        +--Pyrit | local: f0f1913c645b445dd391fb047b812b5ba511782c | remote: f0f1913c645b445dd391fb047b812b5ba511782c
     |--aanarchyy
-        +--bully | local: c830...193b | remote: 3ab3...f521
+        +--bully | local: 3ab3bc830738f447dce112e8551e3ac8193bf521 | remote: 3ab3bc830738f447dce112e8551e3ac8193bf521
     |--aircrack-ng
-        +--rtl8812au | local: d980...774e | remote: d980...774e
+        +--rtl8812au | local: d98018d038a5db96066e79f26ed4a72f2fe1774e | remote: d98018d038a5db96066e79f26ed4a72f2fe1774e
     |--UNC0V3R3D
-        +--Flipper_Zero-BadUsb | local: 6e71...5e3f | remote: fece...5e68
+        +--Flipper_Zero-BadUsb | local: 6e71fece6db47fe9191b357c8bbfe6af5e685e3f | remote: 6e71fece6db47fe9191b357c8bbfe6af5e685e3f
+==================================================================================================================
+What would you like to do?
+1. Clone a repository
+2. Quit the tool
+> 1
+Please enter the GitHub username:
+> norad-defunc
+Please enter the repository name:
+> DB
+Cloning into '/home/joshua/Code/norad-defunc/DB'...
+remote: Enumerating objects: 61, done.
+remote: Counting objects: 100% (61/61), done.
+remote: Compressing objects: 100% (55/55), done.
+remote: Total 61 (delta 0), reused 58 (delta 0), pack-reused 0
+Receiving objects: 100% (61/61), 944.79 KiB | 3.90 MiB/s, done.
+==================================================================================================================
 ```
 
 ## Credits
--- gcm.py
+++ gcm.py
@@ -5,6 +5,7 @@ import sys
 import subprocess
 import requests
 
+# Define a function to clone a GitHub repository
 def clone_repo(github_username, repo_name):
     code_dir = os.path.expanduser('~/Code')
     user_dir = os.path.join(code_dir, github_username)
@@ -18,25 +19,35 @@ def clone_repo(github_username, repo_name):
     subprocess.run(['git', 'clone', f'https://github.com/{github_username}/{repo_name}.git', repo_dir])
 
 
+# Define a function to display the directory tree of cloned repositories
 def show_code_tree():
+    print('\033[1;33m' + '==================================================================================================================' + '\033[0m')
     code_dir = os.path.expanduser('~/Code')
     print('\033[1;37m' + code_dir + '\033[0m')
     print('\033[1;37m' + '    |')
 
     for github_username in os.listdir(code_dir):
         user_dir = os.path.join(code_dir, github_username)
-        print('\033[1;37m' + '    |--' + '\033[1;31m' + github_username)
+        print('\033[1;37m' + '    |--' + '\033[1;31m' + github_username + '\033[0m')
 
         for index, repo_name in enumerate(os.listdir(user_dir)):
             repo_dir = os.path.join(user_dir, repo_name)
             print('        \033[1;37m', end='')
             if index == len(os.listdir(user_dir)) - 1:
-                print('+--', end="")
+                print('+--'  + '\033[0m', end="")
             else:
-                print('|--', end='')
-            print('\033[1;32m' + repo_name + "\033[1;37m | local: " + get_latest_commit_local(github_username, repo_name)+ " | remote: " + get_latest_commit(github_username, repo_name))
+                print('|--'  + '\033[0m', end='')
+
+            # Get the latest commit checksums for the local and remote repositories
+            local_commit = get_latest_commit_local(github_username, repo_name)
+            remote_commit = get_latest_commit(github_username, repo_name)
+            # Print the repository name, local and remote commit checksums
+            print('\033[1;32m' + f'{repo_name} | local: {local_commit} | remote: {remote_commit}' + '\033[0m')
+
+    print('\033[1;33m' + '==================================================================================================================' + '\033[0m')
 
 
+# Define a function to get the latest commit checksum of a GitHub repository
 def get_latest_commit(github_username, repo_name):
     url = f'https://api.github.com/repos/{github_username}/{repo_name}/commits'
     response = requests.get(url)
@@ -50,9 +61,9 @@ def get_latest_commit(github_username, repo_name):
     else:
         return "None"
 
-
+# Define a function to get the latest commit checksum of a local GitHub repository
 def get_latest_commit_local(github_username, repo_name):
-    repo_dir = f'~/Code/{github_username}/{repo_name}'
+    repo_dir = os.path.expanduser(f'~/Code/{github_username}/{repo_name}')
     cmd = f'cd {repo_dir} && git rev-parse HEAD'
     result = subprocess.run(cmd, stdout=subprocess.PIPE, shell=True)
 
@@ -61,37 +72,47 @@ def get_latest_commit_local(github_username, repo_name):
     else:
         return "None"
 
-
+# Define a function to display the tool's title
 def display_title():
-    print('''   ______ _  __     ______            __         __  ___                                       
+    print('\033[1;33m' + '==================================================================================================================' + '\033[0m')
+    print('''\033[1;37m   ______ _  __     ______            __         __  ___                                       
   / ____/(_)/ /_   / ____/____   ____/ /___     /  |/  /____ _ ____   ____ _ ____ _ ___   _____
  / / __ / // __/  / /    / __ \ / __  // _ \   / /|_/ // __ `// __ \ / __ `// __ `// _ \ / ___/
 / /_/ // // /_   / /___ / /_/ // /_/ //  __/  / /  / // /_/ // / / // /_/ // /_/ //  __// /    
 \____//_/ \__/   \____/ \____/ \__,_/ \___/  /_/  /_/ \__,_//_/ /_/ \__,_/ \__, / \___//_/     
-                                                                          /____/               
-                                            proudly brought to you by c0d3ki113r
-    ''')
+                                                                          /____/               \033[0m''')
 
-
+# Define the main function to handle command line input and prompts
 def main():
+    # Display the tool's title
     display_title()
-    if len(sys.argv) < 2:
-        print("Usage: python git_code_manager.py <command> [parameters]")
-        return
 
-    command = sys.argv[1]
-
-    if command == "clone":
-        if len(sys.argv) < 4:
-            print("Usage: python git_code_manager.py clone <username> <repository>")
-            return
-        username = sys.argv[2]
-        repository = sys.argv[3]
-        clone_repo(username, repository)
-    elif command == "list":
+    while True:
+        # Always display the list of repositories
         show_code_tree()
-    else:
-        print("Unknown command:", command)
+        # Prompt the user for input
+        print("What would you like to do?")
+        print("1. Clone a repository")
+        print("2. Quit the tool")
+        choice = input("> ")
+
+        if choice == "1":
+            # Prompt the user for the GitHub username and repository name to clone
+            print("Please enter the GitHub username:")
+            github_username = input("> ")
+            print("Please enter the repository name:")
+            repo_name = input("> ")
+
+            # Clone the repository
+            clone_repo(github_username, repo_name)
+        elif choice == "2":
+            # Quit the tool
+            print("Quitting Git Code Manager")
+            return
+        else:
+            # Invalid input
+            print("Invalid input. Please try again.")
+
 
 
 if __name__ == "__main__":
